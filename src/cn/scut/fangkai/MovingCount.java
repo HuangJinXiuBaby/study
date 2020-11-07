3ca3cd2fb23febd6696741292ed001161bbb4053
HuangJinXiuBaby <1796937821@qq.com>
2020-11-07 21:47:27 +0800

    first commit

---
+++ src/cn/scut/fangkai/MovingCount.java
@@ -0,0 +1,18 @@
+package cn.scut.fangkai;
+
+public class MovingCount {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 机器人的运动范围
+     * @param threshold
+     * @param rows
+     * @param cols
+     * @return
+     */
+    public static int movingCount(int threshold, int rows, int cols) {
+        return 0;
+    }
+}
